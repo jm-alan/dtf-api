f1faa3f91b2ffa71bdbe4bc54741d2a4f9d6892b
Joshua <j.maxwell.alan@gmail.com>
2021-04-03 15:31:05 -0400

    create user seed

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,15 +1,37 @@
-from werkzeug.security import generate_password_hash
 from app.models import db, User
+from faker.providers.person.en import Provider
+from random import shuffle, seed
+
+first_names = list(set(Provider.first_names))
+
+seed(4321)
+shuffle(first_names)
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
 
-    demo = User(username='Demo', email='demo@aa.io',
-                password='password')
+  users = []
 
-    db.session.add(demo)
+  demo = User(
+    first_name='Demo',
+    email='demo@aa.io',
+    password='password',
+    max_pins=25,
+  )
 
-    db.session.commit()
+  users.append(demo)
+
+  for i in range(99):
+    users.append(User(
+      first_name=first_names[i],
+      password='password',
+      email=f'{first_names[i]}@fakeuser.com',
+      max_pins=25
+    ))
+
+  db.session.add(users)
+
+  db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
