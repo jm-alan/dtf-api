5fc1d31b95a5d358fdd8a5920ffbdd5fe24b7fed
Joshua <j.maxwell.alan@gmail.com>
2021-04-03 15:31:18 -0400

    finish user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,29 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from .attending import attending
+from .notification import notification
+from .roster_entry import roster_entry
 
 class User(db.Model, UserMixin):
   __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
+  id = db.Column(db.Integer, primary_key=True)
+  max_pins = db.Column(db.Integer, nullable=False)
+  first_name = db.Column(db.String(40), nullable=False)
+  default_locale = db.Column(db.String(255), nullable=True)
+  hashed_password = db.Column(db.String(255), nullable=False)
+  email = db.Column(db.String(255), nullable=False, unique=True)
+  avatar_id = db.Column(db.Integer, db.ForeignKey('images.id'))
+
+  avatar = db.relationship('Image', back_populates='users')
+  hosted_events = db.relationship('Event', back_populates='host')
+  comments = db.relationship('EventPost', back_populates='poster')
+  sent_messages = db.relationship('Message', back_populates='sender')
+  owned_convos = db.relationship('Conversation', back_populates='owner')
+  attending_events = db.relationship('Event', secondary=attending)
+  chatting_convos = db.relationship('Conversation', secondary=roster_entry)
+  unread_conversations = db.relationship('Conversation', secondary=notification)
 
 
   @property
@@ -25,9 +40,18 @@ class User(db.Model, UserMixin):
     return check_password_hash(self.password, password)
 
 
-  def to_dict(self):
+  def to_private(self):
     return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
+      'id': self.id,
+      'firstName': self.first_name,
+      'email': self.email,
+      'defaultLocale': self.default_locale,
+      'maxPins': self.max_pins,
+      'Avatar': self.avatar.to_public()
+    }
+
+  def to_public(self):
+    return {
+      'firstName': self.first_name,
+      'Avatar': self.avatar.to_public()
     }
